c159cf5511469947c629532ef4aede10a0df9443
Yajat <yajatgupta4@gmail.com>
2019-07-08 13:19:13 +0530

    Updates

---
+++ precache-manifest.1b325fc645b19ca327343f20e7418c67.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c56fc4b4bcc342f42fd443c0d37ab7f1",
+    "url": "/Property-List/index.html"
+  },
+  {
+    "revision": "94ef60e2dfe7603d4657",
+    "url": "/Property-List/static/css/main.ee0344be.chunk.css"
+  },
+  {
+    "revision": "8e7f4697892933d3d13d",
+    "url": "/Property-List/static/js/2.ee4acd37.chunk.js"
+  },
+  {
+    "revision": "94ef60e2dfe7603d4657",
+    "url": "/Property-List/static/js/main.008d5fd8.chunk.js"
+  },
+  {
+    "revision": "ac01dfd190e339c01c1f",
+    "url": "/Property-List/static/js/runtime~main.34a8816d.js"
+  },
+  {
+    "revision": "7aeb5355deab04a01e75189149041c3d",
+    "url": "/Property-List/static/media/house2.7aeb5355.jpg"
+  }
+]);
